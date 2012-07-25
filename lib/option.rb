e875182a7db25986c6e048b1739481820dcc36e6
Robbie Clutton <robert.clutton@gmail.com>
2012-07-24 21:45:51 -0400

    some meta programming foo

--- lib/option.rb
+++ lib/option.rb
@@ -20,6 +20,11 @@ class Option
     Option(@values.select { |e| block.call(e) }.first)
   end
 
+  def method_missing(method, *arguments, &block)
+  	result = @values.send method, &block
+  	Option(result)
+  end
+
 end
 
 def Option(value)
@@ -30,5 +35,6 @@ p Option(" foo ").map { |f| f.strip }.fetch { "hello" }
 p Option(nil).map { |f| f.strip }.select { |f| f.length > 0 }.map { |f| f.upcase }.fetch # strange exception
 p Option(nil).map { |f| f.strip }.select { |f| f.length > 0 }.map { |f| f.upcase }.fetch { "hello"}
 p Option(" foo ").map { |f| f.strip }.select { |f| f.length > 0 }.map { |f| f.upcase }
+p Option(" foo ").find { |f| f == " foo " }
 
 
